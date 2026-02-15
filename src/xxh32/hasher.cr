c34de5ce7e29c4212a5845c6332eeaed58d65f58
Werner Stein <werner.stein@gmail.com>
2026-02-15 14:26:13 +0100

    refactor(hasher): separate unseeded and seeded hash overloads for XXH32 and XXH64

--- src/xxh32/hasher.cr
+++ src/xxh32/hasher.cr
@@ -4,19 +4,37 @@ require "../xxh32/state"
 
 module XXH
   module XXH32
-    def self.hash(data : Bytes, seed : Seed32 = 0_u32) : UInt32
+    # Unseeded (default) overloads
+    def self.hash(data : Bytes) : UInt32
+      Bindings::XXH32.hash(data)
+    end
+
+    def self.hash(string : String) : UInt32
+      Bindings::XXH32.hash(string)
+    end
+
+    def self.hash(io : IO) : UInt32
+      Bindings::XXH32.hash(io)
+    end
+
+    def self.hash_file(path : String | Path) : UInt32
+      Bindings::XXH32.hash_file(path)
+    end
+
+    # Seeded overloads
+    def self.hash(data : Bytes, seed : Seed32) : UInt32
       Bindings::XXH32.hash(data, seed)
     end
 
-    def self.hash(string : String, seed : Seed32 = 0_u32) : UInt32
+    def self.hash(string : String, seed : Seed32) : UInt32
       Bindings::XXH32.hash(string, seed)
     end
 
-    def self.hash(io : IO, seed : Seed32 = 0_u32) : UInt32
+    def self.hash(io : IO, seed : Seed32) : UInt32
       Bindings::XXH32.hash(io, seed)
     end
 
-    def self.hash_file(path : String | Path, seed : Seed32 = 0_u32) : UInt32
+    def self.hash_file(path : String | Path, seed : Seed32) : UInt32
       Bindings::XXH32.hash_file(path, seed)
     end
   end
--- src/xxh64/hasher.cr
+++ src/xxh64/hasher.cr
@@ -4,19 +4,37 @@ require "../xxh64/state"
 
 module XXH
   module XXH64
-    def self.hash(data : Bytes, seed : Seed64 = 0_u64) : UInt64
+    # Unseeded (default) overloads
+    def self.hash(data : Bytes) : UInt64
+      Bindings::XXH64.hash(data)
+    end
+
+    def self.hash(string : String) : UInt64
+      Bindings::XXH64.hash(string)
+    end
+
+    def self.hash(io : IO) : UInt64
+      Bindings::XXH64.hash(io)
+    end
+
+    def self.hash_file(path : String | Path) : UInt64
+      Bindings::XXH64.hash_file(path)
+    end
+
+    # Seeded overloads
+    def self.hash(data : Bytes, seed : Seed64) : UInt64
       Bindings::XXH64.hash(data, seed)
     end
 
-    def self.hash(string : String, seed : Seed64 = 0_u64) : UInt64
+    def self.hash(string : String, seed : Seed64) : UInt64
       Bindings::XXH64.hash(string, seed)
     end
 
-    def self.hash(io : IO, seed : Seed64 = 0_u64) : UInt64
+    def self.hash(io : IO, seed : Seed64) : UInt64
       Bindings::XXH64.hash(io, seed)
     end
 
-    def self.hash_file(path : String | Path, seed : Seed64 = 0_u64) : UInt64
+    def self.hash_file(path : String | Path, seed : Seed64) : UInt64
       Bindings::XXH64.hash_file(path, seed)
     end
   end
