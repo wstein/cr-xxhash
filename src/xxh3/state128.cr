67dc13045448221526871d30c465c41cd5763a7c
Werner Stein <werner.stein@gmail.com>
2026-02-15 12:47:48 +0100

    refactor(state): split XXH3 state implementation into state64.cr and state128.cr (preserve loader)

---
+++ src/xxh3/state128.cr
@@ -0,0 +1,48 @@
+require "../bindings/lib_xxh"
+require "../common/errors"
+require "../common/types"
+
+module XXH
+  module XXH3
+    class State128
+      def initialize(seed : Seed64 = 0_u64)
+        @state = LibXXH.XXH3_createState
+        raise StateError.new("Failed to allocate XXH3 state") if @state.null?
+        reset(seed)
+      end
+
+      def update(data : Bytes)
+        update(data.to_slice)
+      end
+
+      def update(slice : Slice(UInt8))
+        ErrorHandler.check!(LibXXH.XXH3_128bits_update(@state, slice.to_unsafe, slice.size), "XXH3_128 update")
+        self
+      end
+
+      def digest : ::XXH::Hash128
+        ::XXH::Hash128.new(LibXXH.XXH3_128bits_digest(@state))
+      end
+
+      def reset(seed : Seed64 = 0_u64)
+        if seed == 0_u64
+          ErrorHandler.check!(LibXXH.XXH3_128bits_reset(@state), "XXH3_128 reset")
+        else
+          ErrorHandler.check!(LibXXH.XXH3_128bits_reset_withSeed(@state, seed), "XXH3_128 reset with seed")
+        end
+        @seed = seed
+        self
+      end
+
+      def dispose
+        return if @state.null?
+        ErrorHandler.check!(LibXXH.XXH3_freeState(@state), "XXH3 free state")
+        @state = Pointer(LibXXH::XXH3_state_t).null
+      end
+
+      def finalize
+        dispose
+      end
+    end
+  end
+end
